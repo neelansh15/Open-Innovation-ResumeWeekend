13db3fe0db39a789faca34bd520004a15e13a458
Neelansh Mathur <neelansh15@outlook.com>
2020-03-21 13:46:56 +0530

    Landing Page Basics done

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-    <Navbar></Navbar>
-    
-    <router-view/>
+    <!-- <div class="box">
+      <Navbar></Navbar>
+      <div class="content">
+        <router-view/>
+      </div>
+    </div> -->
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar'
 
 export default {
   name: 'app',
   components:{
-    Navbar
+    
   }
 }
 </script>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="home">
-
-    <h1>Home</h1>
+<div>
+  <!-- <Navbar></Navbar> -->
+  <div class="home box d-flex justify-content-center align-items-center">
+    <div>
+      <h1>Resume Weekend</h1>
+      <h2>Showcase your personal projects in your own professional portfolio</h2>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
+// import Navbar from '../components/Navbar'
+
 
 export default {
   name: 'Home',
   components: {
-    
+    // Navbar    
   }
 }
 </script>
+
+<style>
+@import '../assets/style.css'
+</style>
